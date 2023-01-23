70000613176a38548040c79c090b0cd2d4985186
AntonPetrov <pasmobiledev@gmail.com>
2023-01-23 23:16:19 +0300

    add image and simple animation

--- app/src/main/java/com/example/petanimation/MainActivity.kt
+++ app/src/main/java/com/example/petanimation/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.petanimation
 
+
+import android.animation.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.petanimation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding()
+    private lateinit var rocket: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initImage()
+
     }
+
+
+    private fun initImage() {
+        rocket = binding.rocket
+        rocket.setOnClickListener { view ->
+            fly()
+
+        }
+    }
+
+    private fun fly() {
+
+        val animator = ObjectAnimator.ofFloat(rocket, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.start()
+    }
+
+
 }
