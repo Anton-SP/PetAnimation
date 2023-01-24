604376a93277991095dfb869f28401563d452eae
toha <toha1988cool@gmail.com>
2023-01-24 11:14:24 +0300

    use motion layout

--- app/src/main/java/com/example/petanimation/MainActivity.kt
+++ app/src/main/java/com/example/petanimation/MainActivity.kt
@@ -1,60 +1,11 @@
 package com.example.petanimation
 
 
-import android.animation.*
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.animation.AccelerateInterpolator
-import android.widget.ImageView
 
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.example.petanimation.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val binding: ActivityMainBinding by viewBinding()
-    private lateinit var rocket: ImageView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initImage()
-
-    }
-
-
-    private fun initImage() {
-        rocket = binding.rocket
-        rocket.setOnClickListener { view ->
-            flyToCorner()
-
-        }
-    }
-
-    private fun fly() {
-
-        val animator = ObjectAnimator.ofFloat(rocket, View.ROTATION, -360f, 0f)
-        animator.duration = 1000
-        animator.start()
-    }
-
-    private fun flyToCorner(){
-        val container = rocket.parent as ViewGroup
-        val containerW = container.width
-        val containerH = container.height
-
-      //  val animator = ObjectAnimator.ofFloat(rocket, View.TRANSLATION_X, 200f)
-        val  moverX = ObjectAnimator.ofFloat(rocket, View.TRANSLATION_X, containerW*0.1f)
-        val  moverY = ObjectAnimator.ofFloat(rocket, View.TRANSLATION_Y, containerH*0.1f)
-        val set = AnimatorSet()
-        set.playSequentially(moverX,moverY)
-        set.duration = 1000
-        set.start()
-
-     //   animator.start()
-    }
-
 
     }
 
